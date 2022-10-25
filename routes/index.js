957a35f96698f258cc8430755b970884cc640af6
Swathy V <svanc212@mail.com>
2022-10-24 20:46:20 -0700

    updated files

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,34 @@
 var  express = require('express');
 var router = express.Router();
 
+const { requiresAuth } = require('express-openid-connect');
+const axios = require('axios');
+
 router.get("/", function(req,res) {
-    let test = req.oidc.isAuthenticated();
+    let isAuthenticated = req.oidc.isAuthenticated();
     res.render("index",{ 
         title: "My auth app", 
-        test1: test
+        isAuthenticated: isAuthenticated
     });
+})
+
+// Trigger the endpoint, and call the middleware, if the user is logged in or out
+router.get('/secure', requiresAuth(), async(req,res) => {
+
+    let data = {}
+    try{
+
+        // calling the server to get the data, makesure you get the data before moving forward(async, await)
+        const apiResponse = await axios.get('http://localhost:5000/public');
+        data = apiResponse.data;
+    }catch(e) {}
+
+    //when there is not error, you will be redirected to the secure page with the data you get prompt api
+    res.render('secure',{
+        title: "Secured Page",
+        isAuthenticated: req.oidc.isAuthenticated(),
+        data
+    })
 })
 
 module.exports = router;
